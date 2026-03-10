2017cc23b6a15c964a690683f649ec7894466f78
Davi Assis <assisdaviyt@gmail.com>
2026-03-10 17:15:13 -0300

    N acabdeikkk

--- script.js
+++ script.js
@@ -3,22 +3,17 @@ document.addEventListener('DOMContentLoaded', () => {
     const sidebar = document.getElementById('sidebar');
     const subBtn = document.getElementById('sub-btn');
 
-    // --- Lógica da Sidebar ---
     if (menuBtn && sidebar) {
         menuBtn.addEventListener('click', () => {
             sidebar.classList.toggle('open');
-            // Toggle na classe do body para o CSS empurrar o layout
             document.body.classList.toggle('sidebar-active');
         });
     }
 
-    // --- Lógica do Botão de Inscrição ---
     if (subBtn) {
-        // Conteúdos das fases do botão baseados no YouTube Real
         const bntInscrito = `<i class="bi bi-bell"></i> Inscrito <i class="bi bi-chevron-down"></i>`;
         const btnPadrao = `Inscrever-se`;
 
-        // 1. Carregar estado salvo do LocalStorage
         const salvo = localStorage.getItem('inscrito') === 'true';
 
         if (salvo) {
@@ -29,7 +24,6 @@ document.addEventListener('DOMContentLoaded', () => {
             subBtn.innerHTML = btnPadrao;
         }
 
-        // 2. Evento de Clique
         subBtn.addEventListener('click', () => {
             const estaInscritoNow = subBtn.classList.toggle('subscribed');
             
